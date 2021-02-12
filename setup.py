57482c23b47b17958bdf7910b66b732181a34c5a
cflerin <ccflerin@gmail.com>
2021-02-12 14:19:03 +0100

    Add requirements.txt and link to setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup, find_packages
 
+def read_requirements(fname):
+    with open(fname, 'r', encoding='utf-8') as file:
+        return [line.rstrip() for line in file]
+
+
 setup(
      name='pycisTopic',
      version='0.1',
      packages=find_packages(),
      include_dirs=["."],
+     install_requires=read_requirements('requirements.txt'),
      author="Carmen Bravo",
      author_email="carmen.bravogonzalezblas@kuleuven.be",
      description="pycisTopic is a Python module to simultaneously identify cell states and cis-regulatory topics from single cell epigenomics data.",
