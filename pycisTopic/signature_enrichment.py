662edf7f2af46bd9a0475df87396d3fc21b8a007
Carmen Bravo <carmen.bravogonzalezblas@kuleuven.be>
2021-09-27 17:36:13 +0200

    Use precomputed rankings with efficient code rather than aucell pandas based ranking process

--- pycisTopic/signature_enrichment.py
+++ pycisTopic/signature_enrichment.py
@@ -1,26 +1,28 @@
 from ctxcore.genesig import GeneSignature
-from pyscenic.aucell import aucell
+from pyscenic.aucell import aucell4r
 from typing import Dict, Union
 
 
 from .utils import *
 from .diff_features import *
 
-def signature_enrichment(input: Union[pd.DataFrame, 'CistopicImputedFeatures'],
-                        region_sets: Dict[str, pr.PyRanges],
+def signature_enrichment(rankings: 'CistopicImputedFeatures',
+                        signatures: Union[Dict[str, pr.PyRanges], Dict[str, List]],
+                        enrichment_type: str = 'region',
                         auc_threshold: float = 0.05,
                         normalize: bool = False,
-                        seed = None,
                         n_cpu: int = 1):
     """
     Get enrichment of a region signature in cells or topics using AUCell (Van de Sande et al., 2020)
     
     Parameters
     ---------
-    input: pd.DataFrame or CistopicImputedFeatures
-        A dataframe with regions as columns and topics/cells and rows; or a CistopicImputedFeatures object
-    region_sets: Dictionary of pr.PyRanges
-        A dictionary containing signatures as pr.PyRanges
+    rankings: CistopicImputedFeatures
+        A CistopicImputedFeatures object with ranking values
+    signatures: Dictionary of pr.PyRanges (for regions) or list (for genes)
+        A dictionary containing region signatures as pr.PyRanges or gene names as list
+    enrichment_type: str
+        Whether features are genes or regions
     auc_threshold: float
         The fraction of the ranked genome to take into account for the calculation of the Area Under the recovery Curve. Default: 0.05
     normalize: bool
@@ -37,23 +39,26 @@ def signature_enrichment(input: Union[pd.DataFrame, 'CistopicImputedFeatures'],
     Van de Sande, B., Flerin, C., Davie, K., De Waegeneer, M., Hulselmans, G., Aibar, S., ... & Aerts, S. (2020). A scalable SCENIC workflow for single-cell gene 
     regulatory network analysis. Nature Protocols, 15(7), 2247-2276.
     """
-    # Format input
-    if isinstance(input, CistopicImputedFeatures):
-        input =  pd.DataFrame.sparse.from_spmatrix(input.mtx.transpose(), columns = input.feature_names, index = input.cell_names)
+    # Compute rankings if needed and format input
+    rankings =  pd.DataFrame(rankings.mtx.transpose(), columns = rankings.feature_names, index = rankings.cell_names)
     # Take regions in input
-    regions_in_input = pr.PyRanges(region_names_to_coordinates(input.columns.tolist()))
-    # Get signatures
-    signatures = [region_set_to_signature(region_sets[key], regions_in_input, key) for key in region_sets.keys()]
+    if enrichment_type == 'region':
+        regions_in_input = pr.PyRanges(region_names_to_coordinates(rankings.columns.tolist()))
+        # Get signatures
+        signatures = [region_set_to_signature(signatures[key], regions_in_input, key) for key in signatures.keys()]
+    if enrichment_type == 'gene':
+        # Get signatures
+        signatures = [gene_set_to_signature(signatures[key], key) for key in signatures.keys()]
     # Run aucell
-    auc_sig = aucell(input, signatures,
-                     num_workers = n_cpu,
-                     auc_threshold = auc_threshold,
-                     noweights = False,
-                     normalize = normalize,
-                     seed = seed)
+    auc_sig = aucell4r(
+              df_rnk = rankings,
+              signatures = signatures,
+              auc_threshold = auc_threshold,
+              noweights = False,
+              normalize = normalize,
+              num_workers = n_cpu)
     auc_sig.columns.names = [None]
     return auc_sig
-
 
 def region_set_to_signature(query_region_set: pr.PyRanges,
                            target_region_set: pr.PyRanges,
@@ -83,4 +88,25 @@ def region_set_to_signature(query_region_set: pr.PyRanges,
                     name = name,
                     gene2weight = dict(zip(query_in_target, weights)))
     return signature
-
+    
+def gene_set_to_signature(gene_set: List,
+                           name: str):
+    """
+    A helper function to generat gene signatures 
+    
+    Parameters
+    ---------
+    gene_set: pr.PyRanges
+        List of genes
+    name: str
+        Name for the signature
+    
+    Return
+    ---------
+        A GeneSignature object to use with AUCell
+    """
+    weights = np.ones(len(query_in_target))
+    signature = GeneSignature(
+                    name = name,
+                    gene2weight = dict(zip(gene_set, weights)))
+    return signature
