Fix var name in normalisation

pycisTopic/diff_features.py
@@ -372,7 +372,7 @@ def normalize_scores(input_mat: Union[pd.DataFrame, 'CistopicImputedFeatures'],
 
     log.info('Normalizing imputed data')
     if isinstance(input_mat, CistopicImputedFeatures):
-        mtx = normalize(imputed_acc_obj.mtx, norm='l1', axis=0)
+        mtx = normalize(input_mat.mtx, norm='l1', axis=0)
         mtx *= scale_factor
         mtx = np.log1p(mtx)
         output = CistopicImputedFeatures(