1a3d3b4bd36f6bad4ad9bf9a9a24f4d7bd8a889d
sdewin <seppe.dewinter@hotmail.com>
2022-08-01 15:36:49 +0200

    update requirements and setup.py for pip install

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import find_packages, setup
 
 
-def read_requirements(fname):
-    with open(fname, 'r', encoding='utf-8') as file:
-        return [line.rstrip() for line in file]
+required = []
+dependency_links = []
 
+# Do not add to required lines pointing to Git repositories
+EGG_MARK = '#egg='
+for line in requirements:
+        if line.startswith('-e git:') or line.startswith('-e git+') or \
+                line.startswith('git:') or line.startswith('git+'):
+                line = line.lstrip('-e ')  # in case that is using "-e"
+                if EGG_MARK in line:
+                        package_name = line[line.find(EGG_MARK) + len(EGG_MARK):]
+                        repository = line[:line.find(EGG_MARK)]
+                        required.append('%s @ %s' % (package_name, repository))
+                        dependency_links.append(line)
+                else:
+                        print('Dependency to a git repository should have the format:')
+                        print('git+ssh://git@github.com/xxxxx/xxxxxx#egg=package_name')
+        else:
+                required.append(line)
 
 setup(
     name='pycisTopic',
@@ -12,7 +27,8 @@ setup(
     setup_requires=['setuptools_scm'],
     packages=find_packages(),
     include_dirs=["."],
-    install_requires=read_requirements('requirements.txt'),
+    install_requires=required,
+    dependency_links=dependency_links,
     author="Carmen Bravo",
     author_email="carmen.bravogonzalezblas@kuleuven.be",
     description="pycisTopic is a Python module to simultaneously identify cell states and cis-regulatory topics from single cell epigenomics data.",
