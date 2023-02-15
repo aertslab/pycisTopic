7c4102d5a17d01980fbfae9c02483a9d0e9ffaae
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2023-02-15 12:25:14 +0100

    Reformat gene annotation docstrings and improve the code slightly.
    Reformat gene annotation docstrings and improve the code slightly
    and fix some other small issues reported by ruff.

--- pycisTopic/gene_annotation.py
+++ pycisTopic/gene_annotation.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Sequence
 
 import pandas as pd
 import polars as pl
@@ -16,25 +17,29 @@ def get_all_gene_annotation_ensembl_biomart_dataset_names(
     ----------
     biomart_host:
         BioMart host URL to use.
-          - Default: "http://www.ensembl.org"
+          - Default: `http://www.ensembl.org`
           - Archived Ensembl BioMart URLs:
-            https://www.ensembl.org/info/website/archives/index.html (List of currently available archives)
+            https://www.ensembl.org/info/website/archives/index.html
+            (List of currently available archives)
 
     Returns
     -------
     Pandas dataframe with all available gene annotation Ensembl BioMart datasets.
 
+    See Also
+    --------
+    pycisTopic.gene_annotation.get_all_gene_annotation_ensembl_biomart_dataset_names
+
     Examples
     --------
     >>> biomart_latest_datasets = get_all_biomart_ensembl_dataset_names(
-    ...    biomart_host="http://www.ensembl.org"
+    ...    biomart_host="http://www.ensembl.org",
     ... )
     >>> biomart_jul2022_datasets = get_all_biomart_ensembl_dataset_names(
-    ...     biomart_host="http://jul2022.archive.ensembl.org/"
+    ...     biomart_host="http://jul2022.archive.ensembl.org/",
     ... )
 
     """
-
     import pybiomart as pbm
 
     biomart_server = pbm.Server(host=biomart_host)
@@ -54,7 +59,8 @@ def get_biomart_dataset_name_for_species(
     Parameters
     ----------
     biomart_datasets:
-        All gene annotation Ensembl BioMart datasets (`get_all_gene_annotation_ensembl_biomart_dataset_names()`).
+        All gene annotation Ensembl BioMart datasets
+        See :func:`pycisTopic.gene_annotation.get_all_gene_annotation_ensembl_biomart_dataset_names`.
     species:
         Species name to search for.
 
@@ -62,13 +68,21 @@ def get_biomart_dataset_name_for_species(
     -------
     Filtered list of Biomart Ensembl dataset names.
 
+    See Also
+    --------
+    pycisTopic.gene_annotation.get_all_gene_annotation_ensembl_biomart_dataset_names
+
     Example
     -------
-    >>> biomart_datasets = get_all_biomart_ensembl_dataset_names(biomart_host="http://www.ensembl.org")
-    >>> get_biomart_dataset_name_for_species(biomart_datasets=biomart_datasets, species="mouse")
+    >>> biomart_datasets = get_all_biomart_ensembl_dataset_names(
+    ...     biomart_host="http://www.ensembl.org",
+    ... )
+    >>> get_biomart_dataset_name_for_species(
+    ...     biomart_datasets=biomart_datasets,
+    ...     species="mouse",
+    ... )
 
-    """
-
+    """  # noqa: W505
     return biomart_datasets[
         biomart_datasets.display_name.str.lower().str.contains(species.lower())
         | biomart_datasets.name.str.lower().str.contains(species.lower())
@@ -78,7 +92,7 @@ def get_biomart_dataset_name_for_species(
 def get_tss_annotation_from_ensembl(
     biomart_name: str,
     biomart_host: str = "http://www.ensembl.org",
-    transcript_type: list[str] | None = ["protein_coding"],
+    transcript_type: Sequence[str] | None = ["protein_coding"],
 ) -> pl.DataFrame:
     """
     Get TSS annotation for requested transcript types from Ensembl BioMart.
@@ -87,27 +101,37 @@ def get_tss_annotation_from_ensembl(
     ----------
     biomart_name
         Ensembl BioMart ID of the dataset.
-        See `get_biomart_dataset_name_for_species()` to get the biomart_name for species of interest, if needed.
-        e.g.: "hsapiens_gene_ensembl", "mmusculus_gene_ensembl", "dmelanogaster_gene_ensembl", ...
+        See :func:`pycisTopic.gene_annotation.get_biomart_dataset_name_for_species`
+        to get the biomart_name for species of interest:
+        e.g.: `hsapiens_gene_ensembl`, `mmusculus_gene_ensembl`,
+              `dmelanogaster_gene_ensembl`, ...
     biomart_host
         BioMart host URL to use.
-          - Default: "http://www.ensembl.org"
+          - Default: `http://www.ensembl.org`
           - Archived Ensembl BioMart URLs:
-            https://www.ensembl.org/info/website/archives/index.html (List of currently available archives)
+            https://www.ensembl.org/info/website/archives/index.html
+            (List of currently available archives)
     transcript_type
-        Only keep list of specified transcript types (e.g.: ["protein_coding"]) or all (None).
+        Only keep list of specified transcript types (e.g.: `["protein_coding"]`) or
+        all (``None``).
 
     Returns
     -------
     Polars DataFrame with TSS positions in BED format.
 
+    See Also
+    --------
+    pycisTopic.gene_annotation.get_biomart_dataset_name_for_species
+
     Examples
     --------
     >>> get_tss_annotation_from_ensembl(biomart_name="hsapiens_gene_ensembl")
-    >>> get_tss_annotation_from_ensembl(biomart_name="hsapiens_gene_ensembl", biomart_host="http://jul2022.archive.ensembl.org/")
+    >>> get_tss_annotation_from_ensembl(
+    ...     biomart_name="hsapiens_gene_ensembl",
+    ...     biomart_host="http://jul2022.archive.ensembl.org/",
+    ... )
 
     """
-
     import pybiomart as pbm
 
     dataset = pbm.Dataset(name=biomart_name, host=biomart_host)
@@ -139,11 +163,7 @@ def get_tss_annotation_from_ensembl(
                 pl.when(pl.col("Strand") == 1)
                 .then(pl.lit("+"))
                 .otherwise(
-                    (
-                        pl.when(pl.col("Strand") == -1)
-                        .then(pl.lit("-"))
-                        .otherwise(".")
-                    )
+                    pl.when(pl.col("Strand") == -1).then(pl.lit("-")).otherwise(".")
                 )
                 .alias("Strand")
             ),
@@ -158,22 +178,31 @@ def get_chrom_alias_mapping(
     ucsc_assembly: str | None = None, chrom_alias_tsv_filename: str | Path | None = None
 ) -> pl.DataFrame:
     """
-    Get chromosome alias mapping from UCSC genome browser for UCSC assembly to be able to map
-    chromosome names between UCSC, Ensembl, GenBank and RefSeq chromosome names or read mapping
-    from local file (chromosome_alias_tsv_file) instead.
+    Get chromosome alias mapping from UCSC genome browser.
+
+    Get chromosome alias mapping from UCSC genome browser for UCSC assembly to be able
+    to map chromosome names between UCSC, Ensembl, GenBank and RefSeq chromosome names
+    or read mapping from local file (chromosome_alias_tsv_file) instead.
 
     Parameters
     ----------
     ucsc_assembly:
-        UCSC assembly names ("hg38", "mm10", "dm6", ...)
+        UCSC assembly names (`hg38`, `mm10`, `dm6`, ...)
         If specified, retrieve data from UCSC.
     chrom_alias_tsv_filename:
-        If specified and `ucsc_assembly` is not `None`, write the chromosome alias mapping to the specified file.
-        If specified and `ucsc_assembly=None`, read chromosome alias mapping from specified file.
+        If specified and ``ucsc_assembly`` is not ``None``, write the chromosome alias
+        mapping to the specified file.
+        If specified and `ucsc_assembly=None`, read chromosome alias mapping from
+        specified file.
 
     Returns
     -------
-    Polars Dataframe with chromosome alias mappings between UCSC, Ensembl, GenBank and RefSeq chromosome names.
+    Polars Dataframe with chromosome alias mappings between UCSC, Ensembl, GenBank and
+    RefSeq chromosome names.
+
+    See Also
+    --------
+    pycisTopic.gene_annotation.get_chrom_alias_mapping
 
     Examples
     --------
@@ -185,16 +214,15 @@ def get_chrom_alias_mapping(
     Get chromosome aliases for hg38 and also write it to a TSV file:
     >>> chrom_alias_hg38_df_pl = get_chrom_alias_mapping(
     ...     ucsc_assembly="hg38",
-    ...     chrom_alias_tsv_filename="chrom_alias_hg38_mapping.tsv"
+    ...     chrom_alias_tsv_filename="chrom_alias_hg38_mapping.tsv",
     ... )
 
     Get chromosome aliases for hg38 from a previous written TSV file:
     >>> chrom_alias_hg38_from_file_df_pl = get_chrom_alias_mapping(
-    ...    chrom_alias_tsv_filename="chrom_alias_hg38_mapping.tsv"
+    ...    chrom_alias_tsv_filename="chrom_alias_hg38_mapping.tsv",
     ... )
 
     """
-
     if ucsc_assembly:
         # Get chromosome alias file from UCSC genome browser.
         chrom_alias_tsv_url_filename = f"https://hgdownload.soe.ucsc.edu/goldenPath/{ucsc_assembly}/bigZips/{ucsc_assembly}.chromAlias.txt"
@@ -221,17 +249,13 @@ def get_chrom_alias_mapping(
                 f"Chromosome alias TSV file with unsupported number of columns ({chrom_alias_df_pl.width})."
             )
 
-        if chrom_alias_tsv_filename and (
-            isinstance(chrom_alias_tsv_filename, str)
-            or isinstance(chrom_alias_tsv_filename, Path)
+        if chrom_alias_tsv_filename and isinstance(
+            chrom_alias_tsv_filename, (str, Path)
         ):
             chrom_alias_df_pl.write_csv(
                 file=chrom_alias_tsv_filename, sep="\t", has_header=True
             )
-    elif chrom_alias_tsv_filename and (
-        isinstance(chrom_alias_tsv_filename, str)
-        or isinstance(chrom_alias_tsv_filename, Path)
-    ):
+    elif chrom_alias_tsv_filename and isinstance(chrom_alias_tsv_filename, (str, Path)):
         chrom_alias_df_pl = pl.read_csv(
             chrom_alias_tsv_filename, sep="\t", has_header=True, comment_char="#"
         )
@@ -243,41 +267,52 @@ def get_chrom_alias_mapping(
     return chrom_alias_df_pl
 
 
-def check_most_likely_chromosome_source_in_bed(
+def find_most_likely_chromosome_source_in_bed(
     chrom_alias_df_pl: pl.DataFrame, bed_df_pl: pl.DataFrame
 ) -> (str, pl.DataFrame):
     """
-    Check which chromosome source (UCSC, Ensembl, GenBank and RefSeq) is
-    the most likely in the provided Polars DataFrame with BED entries.
+    Find which chromosome source is the most likely in the provided BED file entries.
+
+    Find which chromosome source (UCSC, Ensembl, GenBank and RefSeq) given as a
+    ``chrom_alias_df_pl`` Polars DataFrame is the most likely in the provided Polars
+    DataFrame with BED entries.
 
     Parameters
     ----------
     chrom_alias_df_pl
-        Polars DataFrame with UCSC chromosome alias content. See `get_chrom_alias_mapping()`.
+        Polars DataFrame with chromosome alias content.
+        See :func:`pycisTopic.gene_annotation.get_chrom_alias_mapping.get_chrom_alias_mapping`.
     bed_df_pl
-        Polars DataFrame with BED entries. See `read_bed_to_polars_df()`.
+        Polars DataFrame with BED entries.
+        See :func:`pycisTopic.fragments.read_bed_to_polars_df`.
 
     Returns
     -------
+    Tuple of most likely chromosome source and a Polars DataFrame with the ranking of
+    all possible chromosome sources.
 
-    Most likely chromosome source and Polars DataFrame with the ranking of all possible chromosome sources.
+    See Also
+    --------
+    pycisTopic.fragments.read_bed_to_polars_df
+    pycisTopic.gene_annotation.change_chromosome_source_in_bed
+    pycisTopic.gene_annotation.get_chrom_alias_mapping
 
     Examples
     --------
     >>> chrom_alias_hg38_df_pl = get_chrom_alias_mapping(ucsc_assembly="hg38")
     >>> bed_df_pl = read_bed_to_polars_df("test.bed", engine="pyarrow")
-    >>> best_chrom_source_name, chrom_source_stats_df_pl = check_most_likely_chromosome_source_in_bed(
+    >>> best_chrom_source_name, chrom_source_stats_df_pl = find_most_likely_chromosome_source_in_bed(
     ...     chrom_alias_df_pl=chrom_alias_hg38_df_pl,
     ...     bed_df_pl=bed_df_pl,
     ... )
     >>> print(best_chrom_source_name, chrom_source_stats_df_pl)
 
-    """
-
+    """  # noqa: W505
     # Get all unique chromosome names from BED file.
     chroms_from_bed = bed_df_pl.get_column("Chromosome").unique()
 
-    # Check how often those chromosome names are found for each chromosome source (UCSC, Ensembl, GenBank and RefSeq).
+    # Find how often those chromosome names are found for each chromosome source
+    # (UCSC, Ensembl, GenBank and RefSeq).
     chrom_source_stats_df_pl = chrom_alias_df_pl.select(
         [
             pl.col(column_name).is_in(chroms_from_bed).sum()
@@ -286,9 +321,11 @@ def check_most_likely_chromosome_source_in_bed(
     )
 
     # Get the best chromosome source.
-    best_chrom_source_name = chrom_source_stats_df_pl.transpose(
-        include_header=True
-    ).filter(pl.col("column_0") == pl.col("column_0").max())[0, 0]
+    best_chrom_source_name = (
+        chrom_source_stats_df_pl.transpose(include_header=True)
+        .filter(pl.col("column_0") == pl.col("column_0").max())
+        .item()
+    )
 
     return best_chrom_source_name, chrom_source_stats_df_pl
 
@@ -305,20 +342,31 @@ def change_chromosome_source_in_bed(
     Parameters
     ----------
     chrom_alias_df_pl
-        Polars DataFrame with UCSC chromosome alias content. See `get_chrom_alias_mapping()`.
+        Polars DataFrame with UCSC chromosome alias content.
+        See :func:`pycisTopic.gene_annotation.get_chrom_alias_mapping.get_chrom_alias_mapping`.
     bed_df_pl
-        Polars DataFrame with BED entries for which chromosome names need to be remapped
-        from from_chrom_source_name to to_chrom_source_name. See `read_bed_to_polars_df()`.
+        Polars DataFrame with BED entries for which chromosome names need to be
+        remapped from ``from_chrom_source_name`` to ``to_chrom_source_name``.
+        See :func:`pycisTopic.fragments.read_bed_to_polars_df`.
     from_chrom_source_name
-        Current chromosome source name for the input BED file: "ucsc", "ensembl", "genbank" or "refseq".
-        Can be guessed with `check_most_likely_chromosome_source_in_bed()`.
+        Current chromosome source name for the input BED file: `ucsc`, `ensembl`,
+        `genbank` or `refseq`.
+        Can be guessed with
+        :func:`pycisTopic.gene_annotation.find_most_likely_chromosome_source_in_bed`.
     to_chrom_source_name
-        Chromosome source name to which the output Polars DataFrame with BED entries should be mapped:
-        "ucsc", "ensembl", "genbank" or "refseq"
+        Chromosome source name to which the output Polars DataFrame with BED entries
+        should be mapped:
+        `ucsc`, `ensembl`, `genbank` or `refseq`.
 
     Returns
     -------
     Polars Dataframe with BED entries with changed chromosome names.
+
+    See Also
+    --------
+    pycisTopic.fragments.read_bed_to_polars_df
+    pycisTopic.gene_annotation.find_most_likely_chromosome_source_in_bed
+    pycisTopic.gene_annotation.get_chrom_alias_mapping
 
     Examples
     --------
@@ -326,19 +374,21 @@ def change_chromosome_source_in_bed(
     >>> chrom_alias_hg38_df_pl = get_chrom_alias_mapping(ucsc_assembly="hg38")
 
     Get gene annotation for hg38 from Ensembl BioMart.
-    >>> hg38_gene_annotation_bed_df_pl = get_tss_annotation_from_ensembl(biomart_name="hsapiens_gene_ensembl")
+    >>> hg38_gene_annotation_bed_df_pl = get_tss_annotation_from_ensembl(
+    ...     biomart_name="hsapiens_gene_ensembl",
+    ... )
+    >>> hg38_gene_annotation_bed_df_pl
 
-    Change Ensembl chromosome names with UCSC chromosome names in gene annotation for hg38.
+    Replace Ensembl chromosome names with UCSC chromosome names in gene annotation for hg38.
     >>> hg38_gene_annotation_ucsc_chroms_bed_df_pl = change_chromosome_source_in_bed(
     ...     chrom_alias_df_pl=chrom_alias_hg38_df_pl,
     ...     bed_df_pl=hg38_gene_annotation_bed_df_pl,
     ...     from_chrom_source_name="ensembl",
     ...     to_chrom_source_name="ucsc",
     ... )
-    >>> hg38_gene_annotation_bed_df_pl
+    >>> hg38_gene_annotation_ucsc_chroms_bed_df_pl
 
-    """
-
+    """  # noqa: W505
     chrom_source_stats_df_pl = (
         bed_df_pl.join(
             chrom_alias_df_pl.select(
