23638d8de86a971842137abca73c49b2bfb33775
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2025-09-15 14:01:19 +0200

    Convert "Chromosome/scaffold name" and "Gene name" column to str.
    Convert "Chromosome/scaffold name" and "Gene name" column to str
    after querying data from Ensembl with pybiomart (as at least
    chromosome names could be converted to numbers e.g.: 1, 2, 3,..
    but for some e.g. X, Y, MT, ... strings would be used).
    Mixed dtypes for a column messes up Pandas dataframe to Polars
    dataframe conversion.

--- src/pycisTopic/gene_annotation.py
+++ src/pycisTopic/gene_annotation.py
@@ -168,7 +168,7 @@ def get_tss_annotation_from_ensembl(
             "ensembl_gene_id",
         ],
         filters={"transcript_biotype": transcript_type} if transcript_type else None,
-    )
+    ).astype({"Chromosome/scaffold name": str, "Gene name": str})
 
     ensembl_tss_annotation_bed_df_pl = pl.from_pandas(ensembl_tss_annotation).select(
         [
