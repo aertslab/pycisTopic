3c75e6793fca798cee450ecd8123f5e3b6ce7e36
Gert Hulselmans <gert.hulselmans@kuleuven.be>
2026-03-11 15:08:07 +0100

    Add imports to `__init__.py` files of `tmtoolkit_lite` submodule.
    Add imports to `__init__.py` files of `tmtoolkit_lite` submodule.

---
+++ src/pycisTopic/topic_modeling/tmtoolkit_lite/bow/__init__.py
@@ -0,0 +1,3 @@
+from pycisTopic.topic_modeling.tmtoolkit_lite.bow import bow_stats
+
+__all__ = ["bow_stats"]
---
+++ src/pycisTopic/topic_modeling/tmtoolkit_lite/topicmod/__init__.py
@@ -0,0 +1,3 @@
+from pycisTopic.topic_modeling.tmtoolkit_lite.topicmod import evaluate, model_stats
+
+__all__ = ["evaluate", "model_stats"]
